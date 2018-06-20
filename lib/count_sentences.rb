136279b7598512e83ac682e230a71d08a216ac72
Patricia Nykaza <pnykaza@calstatela.edu>
2018-06-20 18:58:15 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else return false 
+  end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else return false 
+  end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else return false 
+  end
   end
 
   def count_sentences
+    
+    without_question_marks = self.gsub("?", ".")
+    without_exclamation_marks = without_question_marks.gsub("!", ".")
 
-  end
+    result = without_exclamation_marks.split(".")
+    result.reject! { |c| c.empty? }
+    
+    return result.count
+    end
 end
